a77d9427afaf265491f26377d35d61bd591f6d65
yanluwei <yanluwei@ghostcloud.cn>
2020-09-17 10:45:08 +0800

    aop

--- src/main/java/cn/ylw/activiti/service/AopService.java
+++ src/main/java/cn/ylw/activiti/service/AopService.java
@@ -1,8 +1,6 @@
 package cn.ylw.activiti.service;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AdviceName;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
